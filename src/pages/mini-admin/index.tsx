aaa2795514476e1034b723dd807176682cef8757
Aleksandr Bekk <aleksandrbekk@bk.ru>
2026-01-11 12:09:20 +0600

    fix: изменен отступ на pt-[100px] как в Layout для школы

--- src/pages/mini-admin/index.tsx
+++ src/pages/mini-admin/index.tsx
@@ -223,7 +223,7 @@ export default function MiniAdmin() {
   ]
 
   return (
-    <div className="min-h-screen bg-black text-white pb-24 pt-14">
+    <div className="min-h-screen bg-black text-white pb-24 pt-[100px]">
       {/* Tabs */}
       <div className="bg-black border-b border-zinc-800">
         <div className="flex overflow-x-auto px-4 py-3 gap-2">
